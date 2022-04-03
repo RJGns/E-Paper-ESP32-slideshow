698cb054e971a6280f931ed7c326f4a53b6dc5f2
Robert Glenhomes <robert.glenholmes@gmail.com>
2022-04-03 03:06:13 +0100

    now in a for loop and added library
    instead of copy pasting, cycle through array

--- Main/Slideshow-main/Slideshow-main.ino
+++ Main/Slideshow-main/Slideshow-main.ino
@@ -5,67 +5,52 @@
 #include "ImagesData.h"
 #include <stdlib.h>
 
+const unsigned char *test[] = {Arduino,ShrekImg,UnionJack};
+const int NumberOfImages = 3;
 const int NextSlideDelay = 10; // in seconds
-UBYTE *BlackImage;
- 
- 
+UBYTE *BlackImage;    
+
 /* Entry point ----------------------------------------------------------------*/
 void setup()
 {
- 
   DEV_Module_Init();
 
-  
   EPD_5IN83_V2_Init();
   EPD_5IN83_V2_Clear();
   DEV_Delay_ms(500);
 
   //Create a new image cache
- 
   /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
   UWORD Imagesize = ((EPD_5IN83_V2_WIDTH % 8 == 0) ? (EPD_5IN83_V2_WIDTH / 8 ) : (EPD_5IN83_V2_WIDTH / 8 + 1)) * EPD_5IN83_V2_HEIGHT;
   if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
     printf("Failed to apply for black memory...\r\n");
-    while(1);
+    while (1);
   }
   printf("Paint_NewImage\r\n");
   Paint_NewImage(BlackImage, EPD_5IN83_V2_WIDTH, EPD_5IN83_V2_HEIGHT, 180, WHITE);
 
+  /*
+    printf("Clear...\r\n");
+    EPD_5IN83_V2_Clear();
 
-
-/*
-  printf("Clear...\r\n");
-  EPD_5IN83_V2_Clear();
-
-  printf("Goto Sleep...\r\n");
-  EPD_5IN83_V2_Sleep();
-  free(BlackImage);
-  BlackImage = NULL;
+    printf("Goto Sleep...\r\n");
+    EPD_5IN83_V2_Sleep();
+    free(BlackImage);
+    BlackImage = NULL;
   */
 }
 
-/* The main loop -------------------------------------------------------------*/
 void loop()
 {
-  
-  // show image for array   
-  printf("show image for array\r\n");
+  //printf("Back to start of slideshow");
   Paint_SelectImage(BlackImage);
   Paint_Clear(WHITE);
   
-  Paint_DrawBitMap(ShrekImg);
-  EPD_5IN83_V2_Display(BlackImage);
-  delay(NextSlideDelay * 1000);
-  //EPD_5IN83_V2_Sleep();
   
-  Paint_DrawBitMap(UnionJack);
-  EPD_5IN83_V2_Display(BlackImage);
-  delay(NextSlideDelay * 1000);
-  //EPD_5IN83_V2_Sleep();
-
-  Paint_DrawBitMap(Arduino);
-  EPD_5IN83_V2_Display(BlackImage);
-  delay(NextSlideDelay * 1000);
- // EPD_5IN83_V2_Sleep();
-
+  for (int i = 0; i <= NumberOfImages; i++) {
+   // printf(array[i]);
+     Paint_DrawBitMap(test[i]);
+     EPD_5IN83_V2_Display(BlackImage);
+     delay(NextSlideDelay * 1000);
+  }
 }
