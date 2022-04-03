996b8c65d23a36287d76d89f21f79464d3008d39
Robert Glenhomes <robert.glenholmes@gmail.com>
2022-04-03 03:24:16 +0100

    Commenting fixes

--- Main/Slideshow-main/Slideshow-main.ino
+++ Main/Slideshow-main/Slideshow-main.ino
@@ -1,26 +1,28 @@
-/* Includes ------------------------------------------------------------------*/
+/* Includes -------------------------------------------------------------------*/
 #include "DEV_Config.h"
 #include "EPD.h"
 #include "GUI_Paint.h"
 #include "ImagesData.h"
 #include <stdlib.h>
 
-const unsigned char *test[] = {Arduino,ShrekImg,UnionJack};
+/*Config ----------------------------------------------------------------------*/
+const unsigned char *ImageArray[] = {Arduino,ShrekImg,UnionJack};
 const int NumberOfImages = 3;
-const int NextSlideDelay = 10; // in seconds
+const int NextSlideDelay = 60; // in seconds
+
+/*Declarations ----------------------------------------------------------------*/
 UBYTE *BlackImage;    
 
 /* Entry point ----------------------------------------------------------------*/
 void setup()
 {
-  DEV_Module_Init();
-
+  //Start display module 
+  DEV_Module_Init(); 
   EPD_5IN83_V2_Init();
   EPD_5IN83_V2_Clear();
   DEV_Delay_ms(500);
 
   //Create a new image cache
-  /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
   UWORD Imagesize = ((EPD_5IN83_V2_WIDTH % 8 == 0) ? (EPD_5IN83_V2_WIDTH / 8 ) : (EPD_5IN83_V2_WIDTH / 8 + 1)) * EPD_5IN83_V2_HEIGHT;
   if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
     printf("Failed to apply for black memory...\r\n");
@@ -28,28 +30,15 @@ void setup()
   }
   printf("Paint_NewImage\r\n");
   Paint_NewImage(BlackImage, EPD_5IN83_V2_WIDTH, EPD_5IN83_V2_HEIGHT, 180, WHITE);
-
-  /*
-    printf("Clear...\r\n");
-    EPD_5IN83_V2_Clear();
-
-    printf("Goto Sleep...\r\n");
-    EPD_5IN83_V2_Sleep();
-    free(BlackImage);
-    BlackImage = NULL;
-  */
 }
 
 void loop()
 {
-  //printf("Back to start of slideshow");
-  Paint_SelectImage(BlackImage);
-  Paint_Clear(WHITE);
   
   
-  for (int i = 0; i <= NumberOfImages; i++) {
-   // printf(array[i]);
-     Paint_DrawBitMap(test[i]);
+  
+  for (int i = 0; i <= NumberOfImages; i++) {   
+     Paint_DrawBitMap(ImageArray[i]);
      EPD_5IN83_V2_Display(BlackImage);
      delay(NextSlideDelay * 1000);
   }
