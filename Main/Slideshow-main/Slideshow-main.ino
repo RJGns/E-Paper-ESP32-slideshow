b5f14430824000c063f549bd38044aeb559685f8
Robert Glenhomes <robert.glenholmes@gmail.com>
2022-04-03 03:59:37 +0100

    Update Slideshow-main.ino

--- Main/Slideshow-main/Slideshow-main.ino
+++ Main/Slideshow-main/Slideshow-main.ino
@@ -9,6 +9,7 @@
 const unsigned char *ImageArray[] = {Arduino,ShrekImg,UnionJack};
 const int NumberOfImages = 3;
 const int NextSlideDelay = 60; // in seconds
+//Highlight this ->  REPLACE_WITH_YOUR_DISPLAY then press CRTL-F or CMD-F
 
 /*Declarations ----------------------------------------------------------------*/
 UBYTE *BlackImage;    
@@ -18,28 +19,26 @@ void setup()
 {
   //Start display module 
   DEV_Module_Init(); 
-  EPD_5IN83_V2_Init();
-  EPD_5IN83_V2_Clear();
+   REPLACE_WITH_YOUR_DISPLAY_Init();
+   REPLACE_WITH_YOUR_DISPLAY_Clear();
   DEV_Delay_ms(500);
 
   //Create a new image cache
-  UWORD Imagesize = ((EPD_5IN83_V2_WIDTH % 8 == 0) ? (EPD_5IN83_V2_WIDTH / 8 ) : (EPD_5IN83_V2_WIDTH / 8 + 1)) * EPD_5IN83_V2_HEIGHT;
+  UWORD Imagesize = (( REPLACE_WITH_YOUR_DISPLAY_WIDTH % 8 == 0) ? ( REPLACE_WITH_YOUR_DISPLAY_WIDTH / 8 ) : ( REPLACE_WITH_YOUR_DISPLAY_WIDTH / 8 + 1)) *  REPLACE_WITH_YOUR_DISPLAY_HEIGHT;
   if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
     printf("Failed to apply for black memory...\r\n");
     while (1);
   }
   printf("Paint_NewImage\r\n");
-  Paint_NewImage(BlackImage, EPD_5IN83_V2_WIDTH, EPD_5IN83_V2_HEIGHT, 180, WHITE);
+  Paint_NewImage(BlackImage,  REPLACE_WITH_YOUR_DISPLAY_WIDTH,  REPLACE_WITH_YOUR_DISPLAY_HEIGHT, 180, WHITE);
 }
 
 void loop()
 {
-  
-  
-  
+  //Loop through ImageArray, then wait for NextSlideDelay, then repeat.
   for (int i = 0; i <= NumberOfImages; i++) {   
      Paint_DrawBitMap(ImageArray[i]);
-     EPD_5IN83_V2_Display(BlackImage);
+     REPLACE_WITH_YOUR_DISPLA_Display(BlackImage);
      delay(NextSlideDelay * 1000);
   }
 }
